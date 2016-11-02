3347bf9ded80cd61d00be5d410f934325ac388d9
George Katsanos <george@gplus.gr>
2016-11-02 23:53:49 +0100

    JSONP solution

--- build/dev-server.js
+++ build/dev-server.js
@@ -11,7 +11,6 @@ var webpackConfig = require('./webpack.dev.conf')
 var port = process.env.PORT || config.dev.port
 // Define HTTP proxies to your custom API backend
 // https://github.com/chimurai/http-proxy-middleware
-var proxyTable = config.dev.proxyTable
 
 var app = express()
 var compiler = webpack(webpackConfig)
@@ -31,15 +30,6 @@ compiler.plugin('compilation', function (compilation) {
     hotMiddleware.publish({ action: 'reload' })
     cb()
   })
-})
-
-// proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
-  if (typeof options === 'string') {
-    options = { target: options }
-  }
-  app.use(proxyMiddleware(context, options))
 })
 
 // handle fallback for HTML5 history API
